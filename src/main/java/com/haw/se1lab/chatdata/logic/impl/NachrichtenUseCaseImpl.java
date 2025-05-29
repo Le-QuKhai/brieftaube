983530d03a2d49c14decce31a16244043c63bed3
ado_mayo1024 <git.hub.leon@gmail.com>
2025-05-29 11:25:35 +0200

    @Autowired Annotation vergessen

--- src/main/java/com/haw/se1lab/chatdata/logic/impl/NachrichtenUseCaseImpl.java
+++ src/main/java/com/haw/se1lab/chatdata/logic/impl/NachrichtenUseCaseImpl.java
@@ -1,7 +1,9 @@
 package com.haw.se1lab.chatdata.logic.impl;
 
+import com.haw.se1lab.chatdata.logic.api.usecase.NachrichtenUseCase;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NachrichtenUseCaseImpl {
+public class NachrichtenUseCaseImpl implements NachrichtenUseCase {
+
 }
