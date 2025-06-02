36f9b206b54caf7cd9b3604675e91c6847ff52b0
infwzj837 <wzj837@haw-hamburg.de>
2025-06-02 12:47:50 +0200

    Verbindungung zu Frontend erstellt mit createBenutzer (Registrierung)

--- src/main/java/com/haw/se1lab/users/facade/api/UserFacade.java
+++ src/main/java/com/haw/se1lab/users/facade/api/UserFacade.java
@@ -2,15 +2,14 @@ package com.haw.se1lab.users.facade.api;
 
 import com.haw.se1lab.users.dataaccess.api.entity.Benutzer;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/user")
 public interface UserFacade {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // defines the HTTP status of the returned HTTP response
-    public void createBenutzer(@RequestBody Benutzer professor);
+    ResponseEntity<?> createBenutzer(@RequestBody Benutzer user);
 }
--- src/main/java/com/haw/se1lab/users/facade/impl/UserFacadeImpl.java
+++ src/main/java/com/haw/se1lab/users/facade/impl/UserFacadeImpl.java
@@ -4,6 +4,7 @@ import com.haw.se1lab.users.dataaccess.api.entity.Benutzer;
 import com.haw.se1lab.users.facade.api.UserFacade;
 import com.haw.se1lab.users.logic.api.usecase.UserUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,12 @@ public class UserFacadeImpl implements UserFacade {
     @Autowired
     private UserUseCase userUseCase;
 
-    public void createBenutzer(Benutzer user){
-        userUseCase.createUser(user);
+    public ResponseEntity<?> createBenutzer(Benutzer user){
+        try {
+            Benutzer benutzer = userUseCase.createUser(user);
+            return ResponseEntity.ok(benutzer);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
