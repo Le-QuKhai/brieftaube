a0e2ae91b1fbba3a495e6a33cca233d128a6574a
e13as <elias.akbari@yahoo.de>
2025-06-03 17:40:38 +0200

    add logger for better debugging

--- src/main/java/com/haw/se1lab/users/facade/impl/UserFacadeImpl.java
+++ src/main/java/com/haw/se1lab/users/facade/impl/UserFacadeImpl.java
@@ -8,16 +8,22 @@ import com.haw.se1lab.users.logic.api.usecase.UserUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 public class UserFacadeImpl implements UserFacade {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserFacadeImpl.class);
+
     @Autowired
     private UserUseCase userUseCase;
 
     public ResponseEntity<?> createBenutzer(RegestrierungsFormular formular){
         try {
             Benutzer benutzer = userUseCase.createUser(formular);
-            System.out.println("Benutzer wurde erfolgreich registriert!"); // Nur zum Testen, ob Benutzer erfolreich erstellt
+            LOG.info("Benutzer '{}' wurde erfolgreich registriert!", benutzer.getBenutzerName());
             return ResponseEntity.ok(benutzer);
         } catch (IllegalArgumentException | RegestrierungsFormularException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
