07d16baf9d52f5326fe56a02905462f847b8fa01
infwzj837 <wzj837@haw-hamburg.de>
2025-06-17 14:09:21 +0200

    Email erstmal aus Benutzer entfernt

--- src/main/java/com/haw/se1lab/users/dataaccess/api/entity/Benutzer.java
+++ src/main/java/com/haw/se1lab/users/dataaccess/api/entity/Benutzer.java
@@ -1,7 +1,6 @@
 package com.haw.se1lab.users.dataaccess.api.entity;
 
 import com.haw.se1lab.general.dataaccess.api.entity.AbstractEntity;
-import com.haw.se1lab.users.common.api.datatype.EmailAdresse;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,13 +17,12 @@ import java.security.Timestamp;
 public class Benutzer extends AbstractEntity
 {
     private String benutzerName;
-    private EmailAdresse emailAdresse;
+    //private EmailAdresse emailAdresse;
     private String password;
     private Timestamp letzerLogin;
 
     public Benutzer(String benutzerName, String password) {
         this.benutzerName = benutzerName;
         this.password = password;
-        this.letzerLogin = letzerLogin;
     }
 }
