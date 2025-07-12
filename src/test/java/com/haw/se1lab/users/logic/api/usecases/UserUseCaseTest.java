b7237a7f1efdf7812d4550996318c379e55cf0df
uncleruckus <wax193@haw-hamburg.de>
2025-07-12 13:48:19 +0200

    TEST FUNKTIONIEREN TEILWEISE, COMMIT nach ANPASSUNG DER FEHLGESCHLAGENEN TESTS und FUCKIN DELETING THIS FUCKINB BINARIES

--- src/test/java/com/haw/se1lab/users/logic/api/usecases/UserUseCaseTest.java
+++ src/test/java/com/haw/se1lab/users/logic/api/usecases/UserUseCaseTest.java
@@ -8,21 +8,16 @@ import com.haw.se1lab.users.common.api.exception.UserDoesntExistsException;
 import com.haw.se1lab.users.dataaccess.api.entity.Benutzer;
 import com.haw.se1lab.users.dataaccess.api.repo.BenutzerRepository;
 import com.haw.se1lab.users.logic.api.usecase.UserUseCase;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.NONE) // environment
-@ExtendWith(SpringExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 public class UserUseCaseTest {
 
@@ -32,26 +27,36 @@ public class UserUseCaseTest {
     @Autowired
     private BenutzerRepository benutzerRepository;
 
-    @BeforeAll
+    // --- SETUP AND TEARDOWN ---
+    // @BeforeEach runs before every single test
+    @BeforeEach
     public void setUp() {
+        // This ensures every test starts with a clean slate and one known user
+        benutzerRepository.deleteAll();
         Benutzer benutzer = new Benutzer("test1", "test");
         benutzerRepository.save(benutzer);
     }
 
-    @AfterAll
+    // @AfterEach is good practice but optional here since @BeforeEach cleans up anyway
+    @AfterEach
     public void tearDown() {
         benutzerRepository.deleteAll();
     }
 
+
+    // --- YOUR TESTS ---
     @Test
-    public void createUserTest() {
-        RegistrierungsFormular benutzer = new RegistrierungsFormular(
+    public void createUserTest() throws RegistrierungsFormularException {
+        // Setup: DB has one user ("test1")
+        RegistrierungsFormular newBenutzerForm = new RegistrierungsFormular(
                 "test2", "test", "test");
-        try {
-            userUseCase.createUser(benutzer);
-        } catch (RegistrierungsFormularException ignored) {
-        }
-        assertTrue(benutzerRepository.findByBenutzerName(benutzer.getBenutzerName()).isPresent());
+
+        // Action
+        userUseCase.createUser(newBenutzerForm);
+
+        // Assertion
+        assertTrue(benutzerRepository.findByBenutzerName("test2").isPresent());
+        // We expect 2 users now: the one from setUp() and the one we just created.
         assertEquals(2, benutzerRepository.findAll().size());
     }
 
@@ -59,11 +64,12 @@ public class UserUseCaseTest {
     public void createUserWithEmptyUsernameTest() {
         RegistrierungsFormular benutzer = new RegistrierungsFormular(
                 "", "test", "test");
-        assertThrows(RegistrierungsFormularException.class, () -> {userUseCase.createUser(benutzer);});
+        assertThrows(RegistrierungsFormularException.class, () -> userUseCase.createUser(benutzer));
     }
 
     @Test
     public void createUserAlreadyExistTest() {
+        // The user "test1" was created in the setUp() method.
         RegistrierungsFormular benutzer = new RegistrierungsFormular("test1", "test", "test");
         assertThrows(RegistrierungsFormularException.class, () -> userUseCase.createUser(benutzer));
     }
@@ -75,25 +81,26 @@ public class UserUseCaseTest {
     }
 
     @Test
-    public void loginTest() {
-        Benutzer benutzer = new Benutzer("test1", "test");
-        try {
-            benutzer = userUseCase.loginBenutzer(benutzer);
-        } catch (IncorrectPasswordException | UserDoesntExistsException ignored) {
-        }
-        assertEquals("test1", benutzer.getBenutzerName());
-        assertEquals("test", benutzer.getPassword());
+    public void loginTest() throws IncorrectPasswordException, UserDoesntExistsException {
+        // The user "test1" with password "test" was created in setUp()
+        Benutzer loginAttempt = new Benutzer("test1", "test");
+
+        Benutzer loggedInBenutzer = userUseCase.loginBenutzer(loginAttempt);
+
+        assertEquals("test1", loggedInBenutzer.getBenutzerName());
+        assertEquals("test", loggedInBenutzer.getPassword());
     }
 
     @Test
     public void loginWrongPasswordTest() {
+        // The user "test1" exists, but the password is wrong
         Benutzer benutzer = new Benutzer("test1", "test2");
         assertThrows(IncorrectPasswordException.class, () -> userUseCase.loginBenutzer(benutzer));
     }
 
     @Test
     public void loginWrongUserNameTest() {
-        Benutzer benutzer = new Benutzer("test", "test");
+        Benutzer benutzer = new Benutzer("non-existent-user", "test");
         assertThrows(UserDoesntExistsException.class, () -> userUseCase.loginBenutzer(benutzer));
     }
 }
